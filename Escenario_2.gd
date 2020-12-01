c160bb2c1e5cb47fb313e37779ad40ccd25fbed9
Cristian <cristian.barraza@ug.uchile.cl>
2020-11-30 23:49:32 -0300

    creoqueahorasi?

--- Escenario_2.gd
+++ Escenario_2.gd
@@ -1,11 +1,17 @@
 extends Spatial
 var scene = preload("res://legoblin.tscn")
-
+var delta=10
 func _input(event):
 	if event.is_action_released("jump"):
 		var spawned_instance = scene.instance()
 		spawned_instance.global_transform.origin=$Spawn.global_transform.origin
 		add_child(spawned_instance)
+
+#	elif event.is_action_released("camara"):
+#		if $Camera1.is_current():
+#			$Camera1.clear_current(true)
+#		else:
+#			$Camera2.clear_current(true)
 
 # Declare member variables here. Examples:
 # var a = 2
